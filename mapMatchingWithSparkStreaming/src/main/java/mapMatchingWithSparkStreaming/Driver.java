64c789ca94bc8f1a17fe09a2a0ee48111a7b4656
Achintya Kumar <achintya-kumar@users.noreply.github.com>
2017-10-07 08:42:13 +0200

    Reimplemented using stateful transformations and serialized RoadMap

--- mapMatchingWithSparkStreaming/src/main/java/mapMatchingWithSparkStreaming/Driver.java
+++ mapMatchingWithSparkStreaming/src/main/java/mapMatchingWithSparkStreaming/Driver.java
@@ -8,7 +8,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.logging.LogManager;
 
 import kafka.serializer.StringDecoder;
 
@@ -45,6 +44,9 @@ import com.bmwcarit.barefoot.matcher.MatcherCandidate;
 import com.bmwcarit.barefoot.matcher.MatcherFactory;
 import com.bmwcarit.barefoot.matcher.MatcherKState;
 import com.bmwcarit.barefoot.matcher.MatcherSample;
+import com.bmwcarit.barefoot.road.BfmapReader;
+import com.bmwcarit.barefoot.roadmap.RoadMap;
+import com.google.common.base.Optional;
 import com.google.gson.Gson;
 
 /**
@@ -57,25 +59,30 @@ public class Driver {
 
 	public static void main(String[] args) throws Exception {
 		Logger log = org.apache.log4j.LogManager.getRootLogger();
-		log.setLevel(Level.WARN);
-		// Initializing SparkConf with 4 threads and StreamingContext with a batch interval of 20 seconds
+		log.setLevel(Level.INFO);
+		
+		// Initializing SparkConf with at least 2 threads and StreamingContext with a batch interval of 5 seconds or more
 		System.out.println("Local execution is DEACTIVATED!");
-		SparkConf conf = new SparkConf().setAppName("spark_kafka")/*.setMaster("local[*]")*/;
+		SparkConf conf = new SparkConf().setAppName("spark_kafka").setMaster("local[*]");
 		JavaSparkContext sc = new JavaSparkContext(conf);
-		JavaStreamingContext ssc = new JavaStreamingContext(sc, Durations.milliseconds(12000));
+		JavaStreamingContext ssc = new JavaStreamingContext(sc, Durations.milliseconds(4000));
+		ssc.checkpoint("/tmp");
 
 		// Create table if it doesn't exist
-		Configuration hBaseConfiguration = HBaseConfiguration.create();
-		hBaseConfiguration.addResource("/etc/hbase/conf/core-site.xml");
-		hBaseConfiguration.addResource("/etc/hbase/conf/hbase-site.xml");
+		Configuration hBaseConfiguration = new HBaseConfiguration();
+		hBaseConfiguration.clear();
+		//hBaseConfiguration.addResource("/etc/hbase/conf/core-site.xml");
+		//hBaseConfiguration.addResource("/etc/hbase/conf/hbase-site.xml");
+		hBaseConfiguration.set("hbase.zookeeper.quorum", "localhost");
+		hBaseConfiguration.set("hbase.zookeeper.property.clientPort","2181");
 		initializeHBaseTable(hBaseConfiguration);
 
 		JavaHBaseContext hbaseContext = new JavaHBaseContext(sc, hBaseConfiguration);
 		// Broadcasting some utilities
-		Broadcast<BroadcastedUtilities> broadcasted = ssc.sparkContext().broadcast(new BroadcastedUtilities());
+		RoadMap map = RoadMap.Load(new BfmapReader("./oberbayern.bfmap"));
+		System.out.println("MAP LOADING ENDED!");
+		Broadcast<BroadcastedUtilities> broadcasted = ssc.sparkContext().broadcast(new BroadcastedUtilities(map));
 		
-		// Feeding Kafka with samples
-		simpleClient.Client.feedKafka();
 
 		// Kafka streaming
 		Map<String, String> kafkaParams = new HashMap<String, String>();
@@ -86,106 +93,81 @@ public class Driver {
 
 		// Getting streams from Kafka
 		JavaPairInputDStream<String, String> kafkaStreams = KafkaUtils.createDirectStream(ssc, String.class, String.class, StringDecoder.class, StringDecoder.class, kafkaParams, topic);
-		JavaDStream<String> lines = kafkaStreams.map(Tuple2::_2)/*ssc.socketTextStream("localhost", 1111) /*KEPT FOR TESTING PURPOSES!*/;
-		lines.print();
+		JavaDStream<String> lines = kafkaStreams.map(Tuple2::_2)/*ssc.socketTextStream("node3", 1111) /*KEPT FOR TESTING PURPOSES!*/;
+
 		// Creating a pair Dstream with the ID as the key
 		JavaPairDStream<String, String> linesInPairedFormWithID = lines.mapToPair(line -> {
 					JSONObject json = new JSONObject(line);
 					String deviceID = (String) json.get("id");
 					return new Tuple2<>(deviceID, line);
 				});
-		linesInPairedFormWithID.print();
 		
-//
-//		// Grouping the Dstreams by the key
+		// Grouping the Dstreams by the key
 		JavaPairDStream<String, List<String>> linesInPairedFormWithIDinListForm = linesInPairedFormWithID.mapValues(v -> {
 			List<String> inListForm = new ArrayList<>();
 			inListForm.add(v);
 			return inListForm;
 		});
-//
+		
+		
 		JavaPairDStream<String, List<String>> linesInPairedFormWithIDAndGroupedByID = linesInPairedFormWithIDinListForm.reduceByKey((a, b) -> { // <-- Replaced groupByKey with reduceByKey, because LESS SHUFFLING!
 			a.addAll(b);
 			return a;
 		});
-//		
-////		linesInPairedFormWithIDAndGroupedByID.print();
-//	
-		JavaPairDStream<String, List<MatcherSample>> linesInPairedFormWithIDAndGroupedByIDAndValueMappedToMatcherSamples = linesInPairedFormWithIDAndGroupedByID.mapValues(v -> {
-					Iterator<String> iterator = v.iterator();
-					List<MatcherSample> listOfSamples = new ArrayList<>();
-					while (iterator.hasNext()) {
-						listOfSamples.add(new MatcherSample(new JSONObject(iterator.next())));
-					}
-					return listOfSamples;
-		});
-//		
-		JavaPairDStream<String, List<MatcherSample>> linesInPairedFormWithIDAndGroupedByIDAndValueMappedToMatcherSamplesSorted = linesInPairedFormWithIDAndGroupedByIDAndValueMappedToMatcherSamples.mapValues(v -> {
-			Collections.sort(v, (a, b) -> {
+		
+		// Stateful transformation begins here
+		JavaPairDStream<String, Tuple2<String, Long>> deviceIdPairedWithKstate = linesInPairedFormWithIDAndGroupedByID.updateStateByKey((samples, currentKState) -> {
+			if(samples.isEmpty()) {
+				if(currentKState.get()._2() == 0L)  // <-- It was seen recently, but now it isn't sending samples. 
+					return Optional.of(new Tuple2<>(currentKState.get()._1(), System.currentTimeMillis()));  // <-- Tagging this for monitoring; if inactive for long, it must be dropped from the loop.
+				else if(currentKState.get()._2() != 0L) {  // <-- So it has no new samples, and also was tagged for monitoring in some previous cycle.
+					long timeSinceLastSeen = (System.currentTimeMillis() - currentKState.get()._2())/60; // <-- How long has it been since it was last seen?
+					if(timeSinceLastSeen >= 10)  // <-- If it has been 10 minutes since it was last seen, maybe it isn't moving anymore and therefore must be removed from looping
+						return Optional.absent(); // <-- Indicating spark that it must be dropped from the loop.
+					else 
+						return currentKState; // <-- It's not sending new samples, but it has been less than 10 minutes since it was last seen. Therefore keeping it in loop.
+				}
+			}
+			
+			Iterator<String> iterator = samples.get(0).iterator();
+			List<MatcherSample> matcherSamples = new ArrayList<>();
+			while (iterator.hasNext()) {
+				matcherSamples.add(new MatcherSample(new JSONObject(iterator.next())));
+			}
+			
+			Collections.sort(matcherSamples, (a, b) -> {
 				Long aTime = new Long(a.time());
 				Long bTime = new Long(b.time());
 				return aTime.compareTo(bTime);
 			});
 			
-			return v;
+			if(!currentKState.isPresent()) {
+				String kStateJSON = broadcasted.getValue().getBarefootMatcher().mmatch(matcherSamples, 1, 150).toJSON().toString();
+				return Optional.of(new Tuple2<>(kStateJSON, 0L));  // <-- 0 because it is not being considered for monitoring.
+			} else {
+				System.out.println("OLD STATE JSON = " + currentKState.get()._1());
+				MatcherKState state = new MatcherKState(new JSONObject(/*TO CHECK*/currentKState.get()._1()), new MatcherFactory(broadcasted.getValue().getRoadMap()));
+				for (MatcherSample sample : matcherSamples)
+					state.update(broadcasted.getValue().getBarefootMatcher().execute(state.vector(), state.sample(), sample), sample);
+				
+				return Optional.of(new Tuple2<>(state.toJSON().toString(), 0L));  // <-- 0 because it is not being considered for monitoring.
+			}
 		});
-//		
-		linesInPairedFormWithIDAndGroupedByIDAndValueMappedToMatcherSamplesSorted.mapValues(v -> {
-			StringBuffer values = new StringBuffer();
-			v.forEach(v1 -> {
-				try {
-					values.append(v1.toJSON().toString()).append("\n");
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			});
-			return values;
-		}).print();
-//		
-//
-		linesInPairedFormWithIDAndGroupedByIDAndValueMappedToMatcherSamplesSorted.mapValues(v -> "oldKstateJSON = " + broadcasted.getValue().getKstateJSONfromHBase(v.get(0).id())).print();
+				
+				
+		JavaDStream<Tuple2<String, String>> javaDstreamForKState = deviceIdPairedWithKstate.map(v -> new Tuple2<>(v._1, v._2._1)).persist(StorageLevel.MEMORY_AND_DISK());
 		
-		
-		JavaPairDStream<String, MatcherKState> linesInPairedFormWithIDAndReducedByIDAndValueMappedToMatcherKState = linesInPairedFormWithIDAndGroupedByIDAndValueMappedToMatcherSamplesSorted
-				.mapValues(v -> {
-					String oldKstateJSON = broadcasted.getValue().getKstateJSONfromHBase(v.get(0).id());
-					if (oldKstateJSON == null)
-						return broadcasted.getValue().getBarefootMatcher().mmatch(v, 1, 150);
-					else {
-						MatcherKState state = new MatcherKState(new JSONObject(oldKstateJSON), new MatcherFactory(broadcasted.getValue().getRoadMap()));
-						// Updating the existing state retrieved from HBase
-						for (MatcherSample sample : v)
-							state.update(broadcasted.getValue().getBarefootMatcher().execute(state.vector(), state.sample(), sample), sample);
+		JavaDStream<Tuple2<String, String>> javaDstreamForPathTrace = javaDstreamForKState.map(v -> {
 
-						return state;
-					}
-		});
-
-//		The following action saves the rows one by one! Replaced with BulkPut but kept because it works more often than BulkPut!
-//		linesInPairedFormWithIDAndGroupedByIDAndValueMappedToMatcherKState.foreachRDD(rdd -> {
-//			rdd.foreach(value -> {
-//				//println(value._1 + ", " + value._2.toJSON().toString());
-//				broadcasted.getValue().saveKstateJSONtoHBase(new String(value._1), value._2);
-//			});
-//		});
-//		
-//		
-//		// *** The transformation below must be persisted in order to avoid recomputation *** The above transformation cannot be persisted as it is non-serializable
-		JavaDStream<String> javaDstreamForKState = linesInPairedFormWithIDAndReducedByIDAndValueMappedToMatcherKState.map(v -> v._1 + "&" + v._2.toJSON().toString()).persist(StorageLevel.MEMORY_AND_DISK());
-		
-		JavaDStream<String> javaDstreamForPathTrace = javaDstreamForKState.map(v -> {
-			
-			String[] values = v.split("&");
 			// Removing funny characters(e.g. \) from the key
-			String key = values[0].replace("\\", "").trim();
+			String key = v._1.replace("\\", "").trim();
 			
 			// Initializing a new path trace object
 			Trace trace = new Trace();
 			trace.setDeviceID(key);
 			
 			List<Long> timestamps = new ArrayList<>();
-			MatcherKState kState = new MatcherKState(new JSONObject(values[1]), new MatcherFactory(broadcasted.getValue().getRoadMap()));
+			MatcherKState kState = new MatcherKState(new JSONObject(v._2), new MatcherFactory(broadcasted.getValue().getRoadMap()));
 			
 			kState.samples().forEach(s -> timestamps.add(s.time())); // <-- Extracting timestamps from kState, to place inside the pathTrace
 			
@@ -198,11 +180,13 @@ public class Driver {
 			//Gson to json-ize the trace for storage
 			Gson gson = new Gson();
 			
-			return key + "&" + gson.toJson(trace);
+			return new Tuple2<>(key, gson.toJson(trace));
 			
 		});
+		javaDstreamForKState.print();
 		hbaseContext.streamBulkPut(javaDstreamForKState, TableName.valueOf("samples"), new PutFunctionForKState());
 		hbaseContext.streamBulkPut(javaDstreamForPathTrace, TableName.valueOf("results"), new PutFunctionForPathTrace());
+		
 		
 		ssc.start();
 		ssc.awaitTermination();
@@ -265,14 +249,13 @@ public class Driver {
 	}
 
 	
-	public static class PutFunctionForKState implements Function<String, Put> {
+	public static class PutFunctionForKState implements Function<Tuple2<String, String>, Put> {
 
 	    private static final long serialVersionUID = 1L;
 
-	    public Put call(String v) throws Exception {
-	      String[] part = v.split("&"); 					// <-- Breaking into rowKey and the JSON value
-	      String rowKey = part[0].replace("\\", "").trim(); // <-- Chiseling rough edges!
-	      String value = part[1];
+	    public Put call(Tuple2<String, String> v) throws Exception {
+	      String rowKey = v._1.replace("\\", "").trim(); // <-- Chiseling rough edges!
+	      String value = v._2;
 	      Put p = new Put(Bytes.toBytes(rowKey));
 	      p.addColumn(Bytes.toBytes("kstate"), Bytes.toBytes("json"), Bytes.toBytes(value));
 	      return p;
@@ -280,18 +263,18 @@ public class Driver {
 
 	  }
 
-	public static class PutFunctionForPathTrace implements Function<String, Put> {
+	public static class PutFunctionForPathTrace implements Function<Tuple2<String, String>, Put> {
 
 	    private static final long serialVersionUID = 1L;
 
-	    public Put call(String v) throws Exception {
-	      String[] part = v.split("&"); 					// <-- Breaking into rowKey and the JSON value
-	      String rowKey = part[0].replace("\\", "").trim(); // <-- Chiseling rough edges!
-	      String value = part[1];
+	    public Put call(Tuple2<String, String> v) throws Exception {
+	      String rowKey = v._1.replace("\\", "").trim(); // <-- Chiseling rough edges!
+	      String value = v._2;
 	      Put p = new Put(Bytes.toBytes(rowKey));
 	      p.addColumn(Bytes.toBytes("pathTrace"), Bytes.toBytes("json"), Bytes.toBytes(value));
 	      return p;
 	    }
 
 	  }
+	
 }
